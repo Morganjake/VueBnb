a02299f930e008f8ba640ff6021c34ff1949545a
Alezander Elliott <aelliott_ustudent@utilita.co.uk>
2024-10-16 14:00:13 +0100

    Moving laptops
    Moving all minor fixes ive made

--- src/components/HeaderElement.vue
+++ src/components/HeaderElement.vue
@@ -12,20 +12,13 @@
     <body>
         <div class="navbar">
 
-            <div class="logo">
-
-                <p>VueBnB</p>
-
-            </div>
-
+            <p class="logo">VueBnB</p>
 
             <input type="text" placeholder="Search Your Address" />
 
             <input type="time" placeholder="Check in" />
 
             <input type="time" placeholder="Check out" />
-
-
 
         </div>
     </body>
@@ -67,43 +60,48 @@ body{
     column-gap: 20px;
     position: absolute;
     width: 100%;
-    background-color: #33423E;
-    opacity: 50%;
+    background-color: transparent;
     align-items: center;
     justify-content: space-around;
     margin: 0;
     padding: 0 10px;
+    backdrop-filter: blur(100px);
+
 
 }
 
 .logo {
     display: flex;
-    font-size: 24px;
+    font-size: 34px;
     color: #9AFFF9;
-    padding: 0 15px;
+    //padding: 0 15px;
     font-family: "Playwrite GB S", cursive;
+    margin: 10px;
+    text-align: center;
 
 }
 
 .navbar input[type=text] {
-    border: #539987 solid 2px;
+    border: white solid 2px;
     border-radius: 10px;
     padding: 8px;
     font-size: 17px;
     background-color: transparent;
     color: white;
-
+    margin: 0;
 }
 
 .navbar input[type=time] {
 
     display: flex;
-    border: #539987 solid 2px;
+    border: white solid 2px;
     border-radius: 10px;
     padding: 8px 2.5pc;
     font-size: 17px;
     background-color: transparent;
     color: white;
+    color-scheme: dark;
+    margin: 0;
 
 
 }
