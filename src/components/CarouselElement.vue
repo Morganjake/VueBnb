f28f8b99040cd6c150cb38fa98e572de842f5a5b
Jake Morgan <jmorgan_ustudent@utilita.co.uk>
2024-10-24 11:23:47 +0100

    Made house carousel
    Made a carousel for the home screen that generates houses as you go along it

--- src/components/CarouselElement.vue
+++ src/components/CarouselElement.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import {defineProps, getTransitionRawChildren, ref} from "vue"
+import {defineProps, ref} from "vue"
 const props = defineProps({
     homes: {
         required: true,
@@ -23,76 +23,70 @@ const cycle = (direction) => {
     const carousel = document.getElementById("backgroundCarousel")
     cycleDebounce = true
 
-    for (let i = 0; i < 2; i++) {
-        // Creating and setting the image
-        const img = document.createElement("img")
-        img.className = "carouselImage"
+    // Creating and setting the image
+    const img = document.createElement("img")
+    img.className = "carouselImage"
 
-        img.src = props.homes[Math.floor(Math.random() * props.homes.length)]["images"][0]
+    img.src = props.homes[Math.floor(Math.random() * props.homes.length)]["images"][0]
 
-        // Adding image to carousel
-        if (i === 0) {
-            carousel.appendChild(img)
-        }
-        else {
-            carousel.insertBefore(img, carousel.firstChild)
-        }
-    }
-
-    let offset = carousel.children[3].getBoundingClientRect().width / 2
+    // Adding and deleting images to carousel
     if (direction === 1) {
-        totalClicks += 1
-        offset += carousel.children[4].getBoundingClientRect().width / 2
-        offset *= -1
+        carousel.appendChild(img)
+        carousel.removeChild(carousel.firstChild)
     }
     else {
-        totalClicks -= 1
-        offset += carousel.children[2].getBoundingClientRect().width / 2
+        carousel.insertBefore(img, carousel.firstChild)
+        carousel.removeChild(carousel.lastChild)
     }
 
-    const finalPosition = window.innerWidth / 2.0 + offset
-    const increment = offset / 100.0
-    const carouselPos = carousel.getBoundingClientRect().left + carousel.getBoundingClientRect().width / 2
+    // moves the carousel so the middle picture is the same as before pictures were added and deleted
+    carousel.style.left = "50vw"
+    let offset = window.innerWidth / 2
+    offset -= carousel.children[0].getBoundingClientRect().width + carousel.children[1].getBoundingClientRect().width / 2
+    if (direction === -1) {
+        offset -= carousel.children[1].getBoundingClientRect().width / 2 + carousel.children[2].getBoundingClientRect().width + carousel.children[3].getBoundingClientRect().width / 2
+    }
+    carousel.style.left = offset + "px"
+
+    // Slides the picture to the next one
+    let increment = 0
+    if (direction === 1) {
+        increment = (carousel.children[1].getBoundingClientRect().width / 2 + carousel.children[2].getBoundingClientRect().width / 2) / -100
+    } else {
+        increment = (carousel.children[3].getBoundingClientRect().width / 2 + carousel.children[2].getBoundingClientRect().width / 2) / 100
+    }
 
     for (let i = 1; i < 101; i++) {
         setTimeout(_ => {
-            carousel.style.left = carouselPos + increment * (Math.sin(i * Math.PI / 200) ** 3 * 100) + "px"
-        }, i * 7)
+            // Slide the carousel using an Ease In and Out sin wave
+            carousel.style.left = offset + increment * (Math.sin(i * Math.PI / 200) ** 3 * 100) + "px"
+        }, i * 4)
     }
 
     setTimeout(_ => {
-
-        let offset = carousel.getBoundingClientRect().width / 2
-
-        for (let i = 0; i < 2; i++) {
-            if (direction === 1) {
-                offset += carousel.children[0].getBoundingClientRect().width / 2
-                carousel.removeChild(carousel.firstChild)
-            } else {
-                offset -= carousel.children[carousel.children.length - 1].getBoundingClientRect().width / 2
-                carousel.removeChild(carousel.children[carousel.children.length - 1])
-            }
-        }
-
-        carousel.style.left = offset + "px"
         cycleDebounce = false
-    }, 707)
+    }, 400)
+
 }
+
+// Initialises the carousel position
+setTimeout(_ => {
+    const carousel = document.getElementById("backgroundCarousel")
+    console.log(carousel)
+    carousel.style.left = "50vw"
+    let offset = window.innerWidth / 2
+    offset -= carousel.children[1].getBoundingClientRect().width / 2 + carousel.children[2].getBoundingClientRect().width + carousel.children[3].getBoundingClientRect().width / 2
+    carousel.style.left = offset + "px"
+}, 1)
 
 </script>
 
 <template>
+    <div id = "middle"></div>
     <div id="backgroundCarousel">
         <img class="carouselImage" :src="house" alt="house">
         <img class="carouselImage" :src="house2" alt="house">
-        <img class="carouselImage" :src="house2" alt="house">
-        <img class="carouselImage" :src="house" alt="house">
-        <img class="carouselImage" :src="house" alt="house">
-        <img class="carouselImage" :src="house2" alt="house">
         <img class="carouselImage" :src="house3" alt="house">
-        <img class="carouselImage" :src="house2" alt="house">
-        <img class="carouselImage" :src="house3" alt="house">
-        <img class="carouselImage" :src="house" alt="house">
         <img class="carouselImage" :src="house2" alt="house">
         <img class="carouselImage" :src="house" alt="house">
     </div>
@@ -103,18 +97,22 @@ const cycle = (direction) => {
 
 <style>
 
+#middle {
+    position: absolute;
+    height: 100vh;
+    width: 50vw;
+    background-color: whitesmoke;
+}
+
 #backgroundCarousel {
     position: absolute;
-    display: grid;
-    grid-template-columns: auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto;
-    top: 85%;
-    left: 50%;
-    transform: translate(-50%, 0px);
+    display: flex;
+    left: 50vw;
 }
 
 .carouselImage {
-    max-height: 10vh;
-    min-height: 10vh;
+    max-height: 100vh;
+    min-height: 100vh;
     margin: 0;
 
 }
